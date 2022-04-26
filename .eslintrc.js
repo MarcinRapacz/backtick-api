26d34b8ad2a2706cc3e2755a3313a1d12334381e
MarcinRapacz <marcin_04@onet.eu>
2022-04-26 23:51:22 +0200

    build: add prettier & code format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,25 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:node/recommended',
+    'prettier',
+  ],
+  rules: {
+    'node/exports-style': ['error', 'module.exports'],
+    'node/prefer-global/buffer': ['error', 'always'],
+    'node/prefer-global/console': ['error', 'always'],
+    'node/prefer-global/process': ['error', 'always'],
+    'node/prefer-global/url-search-params': ['error', 'always'],
+    'node/prefer-global/url': ['error', 'always'],
+    'node/prefer-promises/dns': 'error',
+    'node/prefer-promises/fs': 'error',
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      { ignores: ['modules'] },
     ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      "plugin:node/recommended",
-    ],
-    rules: {
-      "node/exports-style": ["error", "module.exports"],
-      "node/prefer-global/buffer": ["error", "always"],
-      "node/prefer-global/console": ["error", "always"],
-      "node/prefer-global/process": ["error", "always"],
-      "node/prefer-global/url-search-params": ["error", "always"],
-      "node/prefer-global/url": ["error", "always"],
-      "node/prefer-promises/dns": "error",
-      "node/prefer-promises/fs": "error",
-      "node/no-unsupported-features/es-syntax": [
-        "error",
-        { "ignores": ["modules"] }
-      ]
-  }
+  },
 };
